283c59d15d44c45177898c4a8c02b39117de8f8d
Bogdan Baily <mac-bailbo01@dubbabibo02-mac.station>
2021-11-02 13:45:47 +0000

    First Commit

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MyFirstApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloWorldController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<HelloWorldController> _logger;
+
+        public HelloWorldController(ILogger<HelloWorldController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<string> Get(string name)
+        {
+            await Task.Delay(3000);
+            Request.Headers.TryGetValue("User-Agent", out var header);
+            return $"Hello {name},\nThis request came from:\n{header} ";
+        }
+    }
+}
